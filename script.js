d1ccb8205d2cde9dfefb7c2fa21b4f6e78a61896
CJS26 <chrsan26@bergen.org>
2024-03-23 17:10:38 -0400

    c

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function calculateDistance() {
     const destination = document.getElementById("destination").value;
     const mode = document.getElementById("mode").value;
     const apiKey = "AIzaSyCTjZD6eYxpkpVNu62N6LnvxJ2KW0uFwTI"; // Replace with your Google Maps API key
-    
     const apiUrl = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin}&destinations=${destination}&mode=${mode}&key=${apiKey}`;
     
     fetch(apiUrl)
@@ -13,13 +12,11 @@ function calculateDistance() {
                 const distance = data.rows[0].elements[0].distance.text;
                 // const duration = data.rows[0].elements[0].duration.text;
                 let CO2;
-                if (mode === "car") {
+                if (mode === "driving") {
                     CO2 = distance * (8.89 / 25); // CO2 is in KG
-                } else if (mode === "train") {
+                } else if (mode === "transit") {
                     CO2 = distance * 0.14;
-                } else if (mode === "bus") {
-                    CO2 = distance * 0.28;
-                } else if (mode === "walking" || mode === "biking") {
+                } else if (mode === "walking" || mode === "bicycling") {
                     CO2 = 0;
                 }
                 CO2Text = CO2.toString()
