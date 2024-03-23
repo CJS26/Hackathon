b0da6d47dfd5993851ceba565eda72e7db877f6d
CJS26 <chrsan26@bergen.org>
2024-03-23 10:41:30 -0400

    Co-authored-by: SimoNarwhal <SimoNarwhal@users.noreply.github.com> Co-authored-by: edwcha27 <edwcha27@users.noreply.github.com> Co-authored-by: jaydenargudo <jaydenargudo@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Hackathon
 https://prod.liveshare.vsengsaas.visualstudio.com/join?2342FCD229EB099CB4BBF629B9EE9B9BB7E2
+
+https://htmlcheatsheet.com/
---
+++ events.js
@@ -0,0 +1,5 @@
+let eventsData = []; // List to store event data
+
+function addEvent(event) {
+    eventsData.push(event); // Function to add event to the list
+}
