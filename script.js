979e196166985a4a82ed7780ffd219a21c1c8759
CJS26 <chrsan26@bergen.org>
2024-03-23 18:07:20 -0400

    Co-authored-by: SimoNarwhal <SimoNarwhal@users.noreply.github.com> Co-authored-by: edwcha27 <edwcha27@users.noreply.github.com> Co-authored-by: jaydenargudo <jaydenargudo@users.noreply.github.com>

--- script.js
+++ script.js
@@ -20,7 +20,9 @@ function calculateDistance() {
                 } else if (mode === "walking" || mode === "bicycling") {
                     CO2 = 0;
                 }
+                daysNeededForOneTree = (CO2*16.77); 
                 document.getElementById("distanceResult").innerHTML = `<p>Distance: ${distance.toFixed(2)} miles.</p>`;
+                document.getElementById("").innerHTML = `<p>Your trip will take 1 tree ${daysNeededForOneTree.toFixed(2)} to clear the CO2.</p>`;
         document.getElementById("co2Result").innerHTML = `<p>Your trip will produce an estimated ${CO2.toFixed(2)} KG of CO2.</p>`;
             } else {
                 document.getElementById("distanceResult").innerHTML = `<p>Error: ${data.error_message}</p>`;
